2127c02a293ada29145dfee121def2eccf24690f
1Zui3 <157219200+1Zui3@users.noreply.github.com>
2024-12-10 14:11:55 +0800

    My portfolio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,11 +10,7 @@ function AboutMe() {
       </div>
       <h2>About Me</h2>
       <p>
-<<<<<<< HEAD
-        Hello! I am Samuel B. Balagtas and i am a passionate developer with expertise in React and modern web technologies. I enjoy creating visually appealing and user-friendly web applications.
-=======
-        Hello! I am Samuel B. Balagtas and i am a passionate developer with expertise in React and modern web technologies and i enjoy creating user-friendly websites.
->>>>>>> 6ec175e343671269e5162a52ff1d870e9848d7ee
+        Hello! I am Samuel B. Balagtas, and I am a passionate developer with expertise in React and modern web technologies. I enjoy creating visually appealing and user-friendly web applications.
       </p>
     </section>
   );
